b82ef219c73b2014c4f5f4c7efea2be7825cc31a
Marlo <marlo.baguyos@outlook.com>
2021-07-12 15:28:43 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import NavBar from './components/navBar'
-import Footer from './components/footer';
+import NavBar from './components/navBar';
+import Footer from './components/footer.jsx';
+import QuantityPicker from './components/quantityPicker.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
 		<div className="App">
 			<NavBar></NavBar>
 			<h1>GPU Shuffle!</h1>
+
+			<QuantityPicker></QuantityPicker>
+
 			<Footer></Footer>
 		</div>
   );
---
+++ src/components/quantityPicker.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class QuantityPicker extends Component {
+	render() { 
+		return ( 
+			<div>
+				<label>QP</label>
+			</div>
+		);
+	}
+}
+
+export default QuantityPicker;
