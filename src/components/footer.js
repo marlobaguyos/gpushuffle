b433d52b7a3c1760e94b57e128ba123cfe1b723a
Marlo <marlo.baguyos@outlook.com>
2021-07-12 15:02:24 -0700

    update

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+	
+	render() { 
+		return ( 
+			<React.Fragment>
+				<div>2021 All rights reserved</div>
+				<label>GPUSHUFFLE LLC</label>
+			</React.Fragment>
+		);
+	}
+}
+
+export default Footer;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+	render() { 
+		return ( 
+			<div>
+				NavBar will be here!!! 
+			</div>
+		);
+	}
+}
+
+export default NavBar;
